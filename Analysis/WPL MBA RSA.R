9d7c7ebc3d293d1d44a58ce8dccf060403d51995
lirabenjamin <lira.benjamin@gmail.com>
2021-01-14 14:07:06 -0600

    Added RSAs

--- Analysis/WPL MBA RSA.R
+++ Analysis/WPL MBA RSA.R
@@ -6,13 +6,13 @@ d9 = read.csv("data/WCI_MBA_data_2019.csv")
 d0 = read.csv("data/WCI_MBA_data_2020.csv") 
 
 d = rbind(d9,d0)
-
+d
 d %>% select(StartDate,EndDate)
 
 sc = d %>% select(X,SubjectID,EvaluatorID,Relationship,
                   GritPer_scale,
                   GritPas_scale,
-                  HumMot_scale,
+                  HumMot_scale, 
                   HumRec_scale,
                   Giver_scale,
                   Matcher_scale,
@@ -23,11 +23,16 @@ sc = d %>% select(X,SubjectID,EvaluatorID,Relationship,
 
 ids = d %>% select(X,SubjectID,EvaluatorID,Relationship,year,class)
 
-ids %>% arrange(X)
+ids %>% arrange(EvaluatorID,SubjectID)
+
+# Conversation with Chayce
+# Control for year, not nest for.
+# Compare fixed subjects effect to random slopes of their nested evaluators
+# lmer(dv ~ IV_participant*IV_informant + (ParticipantID | InformantID))
 
 # Descriptives ####
 sc$SubjectID %>% unique() %>% length() #623 unique people were evaluated, 739 total, (116 ppl did it twice)
-sc %>% select(SubjectID) %>% group_by(SubjectID) %>% summarise(n=n()) %>% pull(n) %>% table() #8 evals on avg, 1-43
+sc %>% select(SubjectID) %>% group_by(SubjectID) %>% summarise(n=n()) %>% pull(n) %>% sd() #8.59 (5.35) evals on avg, 1-43
 sc$EvaluatorID %>% unique() %>% length() #3558 unique people were evaluators
 
 # Do people change from year to year? ####
@@ -36,8 +41,12 @@ sc %>%
   mutate(avg_perf = (avg_perf/100)*6+1) %>% 
   select(-SubjectID,-X,-class,-Relationship) %>% 
   gather(varaible,value,-year,-EvaluatorID) %>% 
-  ggplot(aes(year,value))+geom_line(aes(group = EvaluatorID))+
-  facet_wrap(~varaible)
+  ggplot(aes(year,value))+
+  geom_line(aes(group = EvaluatorID),alpha=.3)+
+  facet_wrap(~varaible,nrow = 2)+
+  scale_x_continuous(breaks = c(2019,2020))+
+  scale_y_continuous(breaks = 1:7)+
+  theme_ang()
 
 # Is there variability within? ####
 library(lme4)
@@ -76,21 +85,80 @@ iccs %>% select(year,Outcome,ICC) %>%
 # RSAs ####
 library(RSA)
 ValidIDs = sc %>% 
-  group_by(SubjectID) %>% 
+  mutate(idy = paste0(SubjectID,year)) %>% 
+  group_by(idy) %>% 
   mutate(inclself = Relationship %in% c("Self")) %>% 
   summarise(self = mean(inclself)) %>% 
   filter(self > 0 & self < 1) %>% 
-  pull(SubjectID)
+  pull(idy)
 
 
-sc %>% 
-  select(SubjectID,year,Relationship,GritPas_scale,avg_perf) %>% 
-  filter(SubjectID %in% ValidIDs) %>% 
-  group_by(SubjectID) %>% 
-  mutate(avg_perf_m = mean(avg_perf)) %>% 
-  ungroup() %>% 
-  mutate(rel = case_when(Relationship == "Self"~1,
-                                Relationship != "Self"~0))
+# rsas = sc %>% 
+#   mutate(idy = paste0(SubjectID,year)) %>% 
+#   filter(idy %in% ValidIDs) %>% 
+#   select(-X) %>% 
+#   group_by(SubjectID) %>% 
+#   mutate(p = mean(avg_perf)) %>% 
+#   gather(Variable,o,-SubjectID,-EvaluatorID,-Relationship,-p,-class,-year,-idy) %>% 
+#   group_by(idy,Variable) %>% 
+#   mutate(s = o[Relationship == "Self"]) %>% 
+#   filter(Relationship != "Self") %>% 
+#   group_by(Variable,year) %>% 
+#   mutate(Relationship = as.factor(Relationship),
+#          year = factor(year),
+#          class = factor(class),
+#          o = scale(o),
+#          s = scale(s),
+#          p = scale(p)) %>% 
+#   nest() %>% 
+#   mutate(rsa = map(data,function(x){RSA(formula = p~o*s+Relationship+class, data = x)}))
+
+# write_rds(rsas,"Output/rsas.rds")
+rsas = read_rds("Output/rsas.rds")
+
+extract = function(x){
+  rsas$rsa[[1]]$models$full %>% parameterestimates(standardized = T) %>% 
+    mutate(beta = formatest(std.all,pvalue)) %>% 
+    select(label,std.all,pvalue,beta) %>% 
+    filter(str_detect(label,"a")|str_detect(label,"b"))
+}
+
+rsaparams = rsas %>% 
+  mutate(params = map(rsa,extract),
+         r2 = map_dbl(rsa,"r.squared")) %>% 
+  unnest(params) %>% 
+  select(-data,-rsa,-pvalue,-std.all) %>% 
+  spread(label,beta) %>% 
+  select(-a5)
+colnames(rsaparams)[4:12] = c("SLC","CLC","SLI","CLI","self","other","self2","interaction",'other2')
+rsaparams[c(1:9,11,10,12)] %>% 
+  mutate(r2 = numformat(r2)) %>% 
+  write.clip()
+
+rsas$rsa[[9]] %>% plot()
+
+plots = rsas %>% mutate(plot = map2(rsa,paste(Variable,as.character(year)),function(x,y){plot(x,
+                                                                    xlab = "Self", #Title
+                                                      ylab = "Other",
+                                                      zlab = Variable,
+                                                      type = "3d",         #For those who have difficulty interpreting 3-d figures try "interactive" which will let you rotate the figure
+                                                      surface = "predict", #Response surface is based on predicted values of outcome
+                                                      rotation = list(x = -63, y = 32, z = 15),
+                                                      legend  = FALSE,     #TRUE displays color legend
+                                                      param   = FALSE,      #Display RSA parameters
+                                                      coefs   = FALSE,      #Display polynomial coefficients 
+                                                      axes    = c("LOC", "LOIC"), #Display line of congruence and line of incongruence
+                                                      project = NULL,      #TRUE displays projections onto the bottom of the plot
+                                                      hull    = FALSE,     #TRUE displays a bag plot on the surface
+                                                      points = FALSE)})) %>% 
+  select(year,Variable,plot)
+
+
+for (i in 1:16){
+  png(paste0("Plots/",i,"RSA.png"), res = 500,height = 1500,width = 1500) 
+  print(plots$plot[[i]])
+  dev.off() 
+}
 
 
 # Function: Returns mahalanobis distance for each evaluator.
